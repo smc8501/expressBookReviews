ba660ab3fe40ffdcab0a7a2543b8574fc62eaaf8
smc8501 <s.marijuancarreno9378@gmail.com>
2026-07-16 13:53:02 +0100

    Add /customer/auth/review/:isbn

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,22 +7,75 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+  let filtered_users = users.filter((user) => user.username === username);
+ 
+  return filtered_users.length > 0;
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
-}
-
+  // Filter the users array to find a user with the matching username and password
+  return users.some(
+    user => 
+      user.username === username && 
+      user.password === password
+  );
+};
+ 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (!authenticatedUser(username, password)){
+    return res.status(403).json({message: "User not authenticated"})
+  } 
+  const user = users.find(user => user.username === username);
+
+  const accessToken = jwt.sign(
+    {username: user.username},
+    "access",
+    {expiresIn: 60*60}
+  );
+  // Store token in session
+  req.session.authorization = {
+    accessToken
+  };
+
+  return res.status(200).json({
+    message: "User logged in successfully",
+    accessToken
+  })
+
 });
 
+
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.put("/auth/review/:isbn",  (req, res) => {
+  const user_authed = req.user.username;
+  const ISBN = req.params.isbn;
+  let details = req.body.reviews;
+
+  if (!books[ISBN]) {
+    return res.status(404).json({
+      message: "Book not found"
+    });
+  }
+  const existingReview = books[ISBN].reviews.find(
+    review => review.user === username
+  );
+  if (existingReview) {
+    return res.status(400).json({
+      message: "You have already reviwed this book"
+    });
+  }
+
+  books[ISBN].reviews.push({user:user_authed, reviews:details});
+
+  return res.status(201).json({message: "Review added successfully"})
+
+  
 });
 
 module.exports.authenticated = regd_users;
